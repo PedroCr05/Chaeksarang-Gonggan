fc1de58c352528256638feb71c7562868800dfbf
pedrocruz <JayJMarquez05@gmail.com>
2024-12-10 09:59:13 -0600

    I'll ignore the styling for now. Models must be worked on today

--- main_app/models.py
+++ main_app/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class Stories(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=150)
+    story = models.TextField
+    tags = models.ManyToOneRel
+    photo = models.CharField
+    
