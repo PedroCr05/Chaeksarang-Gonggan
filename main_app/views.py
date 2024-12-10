9862e38cf34325970f34f3b4baaa66f935f67987
pedrocruz <JayJMarquez05@gmail.com>
2024-12-09 19:38:08 -0600

    Got some styling going. Finally got what I wanted with my about page. Just 1 more thing but will leave it for tommorrow

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+class Stories:
+    def __init__(self, name, description, story, tags, photo):
+        self.name = name,
+        self.description = description,
+        self.story = story,
+        self.tags = tags,
+        self.photo = photo,
+
+stories = [
+    Stories('name', 'description', 'story', 'tags', 'photo url'),
+    Stories('Far from her', 'A world where I am away from her', 'Once a upon... the end', 'romance', 'url.com'),
+    Stories('A cup', 'A story about a blue cup', 'The cup was gray and it is blue. The end', 'comedy', 'url.com' ),
+    Stories('good guy', 'he once was a bad guy', 'He was evil but now turned to good', 'superhero', 'photourl.com')
+]
 
 def home(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def story_index(request):
+    return render(request, 'story/index.html', {'stories': stories})
