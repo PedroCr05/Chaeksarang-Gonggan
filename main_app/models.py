75994704b2b15d9a677f3e8f8eab5a8f35b30aad
pedrocruz <JayJMarquez05@gmail.com>
2024-12-11 11:00:16 -0600

    stories page works now. Wasn't able to push yesterday internet wasn't able to continue on with no internet.

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Stories(models.Model):
+class Story(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=150)
     story = models.TextField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Stories
+from .models import Story
 
 def home(request):
     return render(request, 'home.html')
@@ -8,9 +8,9 @@ def about(request):
     return render(request, 'about.html')
 
 def story_index(request):
-    stories = Stories.objects.all()
+    stories = Story.objects.all()
     return render(request, 'stories/index.html', {'stories': stories})
 
-def story_details(request):
-    stories = Stories.objects.get(id=story_id)
-    return render(request, 'stories/detail.html', {'stories': stories})
+def story_details(request, story_id):
+    story = Story.objects.get(id=story_id)
+    return render(request, 'stories/detail.html', {'story': story})
